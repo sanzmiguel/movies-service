b03763daf363e8226107042bb9bd5c2c6fb8ec5a
Miguel Sanz <sanzmartinmiguel@gmail.com>
2019-07-06 18:31:57 +0200

    Returning response

--- app/routes/movies.router.js
+++ app/routes/movies.router.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const httpStatus = require('http-status');
 const moviesDomain = require('../domain/movies');
 
 const router = express.Router();
 
-router.get('/', moviesDomain.getMovies);
+const getMovies = (req, res) => {
+  const movies = moviesDomain.getMovies();
+  res.status(httpStatus.OK).send({ data: movies, status: httpStatus.OK });
+};
+
+router.get('/', getMovies);
 
 module.exports = router;
