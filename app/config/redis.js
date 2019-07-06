ab91459c4c19e1526f45457f4cbb5c76be0cca59
Miguel Sanz <sanzmartinmiguel@gmail.com>
2019-07-06 23:15:43 +0200

    Setted environmental variable

--- app/config/redis.js
+++ app/config/redis.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.REDIS_HOST || 'redis',
+  host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || '6379',
   prefix: process.env.REDIS_PREFIX || 'MOVIES',
   expireTime: process.env.REDIS_EXPIRE_TIME || 600,
